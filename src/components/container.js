841fb4d53a56c5e49af1eedf72d19fb5e286c83a
Jackson Booth <jacksontbooth@icloud.com>
2022-01-14 01:40:24 +1100

    added component container

---
+++ src/components/container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './container.css'
+
+class Container extends Component {
+    render() {
+        return(
+            <div class="container">
+            </div>
+        )
+    }
+}
+
+export default Container
